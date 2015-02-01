ef6c9eb4f6a592d33f4c582fa4b21b42d06446c4
RobBobC <rjcook02@louisville.edu>
2015-02-01 18:06:08 -0500

    fix Nathan's stupidity

--- Assets/LeapMotion/DemoResources/Scripts/HandCycler.cs
+++ Assets/LeapMotion/DemoResources/Scripts/HandCycler.cs
@@ -6,82 +6,43 @@
 
 using UnityEngine;
 using System.Collections;
-using Leap;
 
-public class HandCycler : MonoBehaviour {
+public class HandCycler : MonoBehaviour
+{
 
-  public HandModel[] leftHands;
-  public HandModel[] rightHands;
+    public HandModel[] leftHands;
+    public HandModel[] rightHands;
 
-  Leap.Controller mc;
+    private int hand_index_ = 0;
 
-  private int hand_index_ = 0;
-
-  void Start() {
-    SetNewHands();
-      mc = new Controller();
-      mc.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
-      Listener ll = new Leap.Listener();
-      mc.AddListener(ll);
-  }
-
-  protected void SetNewHands() {
-    HandController controller = GetComponent<HandController>();
-    controller.leftGraphicsModel = leftHands[hand_index_];
-    controller.rightGraphicsModel = rightHands[hand_index_];
-  }
-
-  void OnGUI() {
-    Event current_event = Event.current;
-    if (current_event.type == EventType.KeyDown) {
-      if (current_event.keyCode == KeyCode.LeftArrow) {
-        hand_index_ = (hand_index_ + leftHands.Length - 1) % leftHands.Length;
+    void Start()
+    {
         SetNewHands();
-      }
-      else if (current_event.keyCode == KeyCode.RightArrow) {
-        hand_index_ = (hand_index_ + 1) % leftHands.Length;
-        SetNewHands();
-      }
     }
-  }
 
-  void Update()
-  {
-      Frame frame = mc.Frame();
-      foreach (Gesture gesture in frame.Gestures())
-      {
-          switch (gesture.Type)
-          {
-              case (Gesture.GestureType.TYPECIRCLE):
-                  {
-                      Debug.Log("Circle gesture recognized.");
-                      break;
-                  }
-              case (Gesture.GestureType.TYPEINVALID):
-                  {
-                      Debug.Log("Invalid gesture recognized.");
-                      break;
-                  }
-              case (Gesture.GestureType.TYPEKEYTAP):
-                  {
-                      Debug.Log("Key Tap gesture recognized.");
-                      break;
-                  }
-              case (Gesture.GestureType.TYPESCREENTAP):
-                  {
-                      Debug.Log("Screen tap gesture recognized.");
-                      break;
-                  }
-              case (Gesture.GestureType.TYPESWIPE):
-                  {
-                      Debug.Log("Swipe gesture recognized.");
-                      break;
-                  }
-              default:
-                  {
-                      break;
-                  }
-          }
-      }
-  }
+    protected void SetNewHands()
+    {
+        HandController controller = GetComponent<HandController>();
+        controller.leftGraphicsModel = leftHands[hand_index_];
+        controller.rightGraphicsModel = rightHands[hand_index_];
+        controller.DestroyAllHands();
+    }
+
+    void OnGUI()
+    {
+        Event current_event = Event.current;
+        if (current_event.type == EventType.KeyDown)
+        {
+            if (current_event.keyCode == KeyCode.LeftArrow)
+            {
+                hand_index_ = (hand_index_ + leftHands.Length - 1) % leftHands.Length;
+                SetNewHands();
+            }
+            else if (current_event.keyCode == KeyCode.RightArrow)
+            {
+                hand_index_ = (hand_index_ + 1) % leftHands.Length;
+                SetNewHands();
+            }
+        }
+    }
 }
