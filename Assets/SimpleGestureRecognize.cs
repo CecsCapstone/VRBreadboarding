59cc9e5583526ec5400f3714dca76e67d16a9d98
nltcardfan1 <nltcardfan1@gmail.com>
2015-02-02 19:03:48 -0500

    added simple gesture recognizer to build off of

---
+++ Assets/SimpleGestureRecognize.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class SimpleGestureRecognize : MonoBehaviour {
+    public Controller controller;
+    public Frame frame;
+	// Use this for initialization
+	
+    void Start () {
+        controller = new Controller();
+        controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+        controller.EnableGesture(Gesture.GestureType.TYPE_SCREEN_TAP);
+        controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
+        controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        frame = controller.Frame();
+        GestureList gestures = frame.Gestures();
+        if(gestures.Count != 0)
+        {
+            foreach (Gesture gest in gestures)
+                Debug.Log(gest.Type.ToString());
+        }
+        
+	}
+}
