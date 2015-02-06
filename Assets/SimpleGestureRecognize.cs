cbbe860b14e9fed48e42840e9e0d04a1367986aa
nltcardfan1 <nltcardfan1@gmail.com>
2015-02-06 17:50:31 -0500

    it's broken. Deal with it

--- Assets/SimpleGestureRecognize.cs
+++ Assets/SimpleGestureRecognize.cs
@@ -22,7 +22,38 @@ public class SimpleGestureRecognize : MonoBehaviour {
         if(gestures.Count != 0)
         {
             foreach (Gesture gest in gestures)
-                Debug.Log(gest.Type.ToString());
+			{
+				if(gest.Type == Gesture.GestureType.TYPE_KEY_TAP)
+				{
+					Debug.Log (gest.Hands.Frontmost.Fingers[1].TipPosition);
+					Leap.Vector fingerPosition = gest.Hands.Frontmost.Fingers[1].TipPosition;
+					float x = fingerPosition.x;
+					float y = fingerPosition.y;
+					float z = fingerPosition.z;
+					Vector3 position = new Vector3(x,y,z);
+
+					GrabbableObject closest = GetComponent<GrabbableObject>();
+					float closestDistance = Mathf.Infinity;
+					GrabbableObject[] listOfGameObjects = new GrabbableObject[10];
+					listOfGameObjects = GetComponents<GrabbableObject>();
+
+					for(int i = 0; i < 10; i++)
+					{
+						Debug.Log (i);
+						if(listOfGameObjects[i] != null)
+						{
+							Debug.Log(listOfGameObjects[i]);
+							float dist = Vector3.Distance (position, listOfGameObjects[i].transform.position);
+							if(closestDistance > dist)
+							{
+								closest = listOfGameObjects[i];
+							}
+						}
+					}
+					Debug.Log (closest.ToString());
+				}
+			}
+                //Debug.Log(gest.Type.ToString());
         }
         
 	}
