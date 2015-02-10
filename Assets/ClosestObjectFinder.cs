a36836781c7a654d7a03a0a41cec336942cfe521
nltcardfan1 <nltcardfan1@gmail.com>
2015-02-10 15:22:20 -0500

    check in

--- Assets/ClosestObjectFinder.cs
+++ Assets/ClosestObjectFinder.cs
@@ -5,47 +5,44 @@ using Leap;
 public class ClosestObjectFinder : MonoBehaviour {
 	public Controller controller;
 	public Frame frame;
-	public InteractionBox iBox;
 
 	// Use this for initialization
 	void Start () {
 		controller = new Controller();
-		iBox = new InteractionBox ();
-		iBox.Center.x = -0.5f;
-		iBox.Center.y = .891f;
-		iBox.Center.z = -.447f;
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 				frame = controller.Frame ();
 
-		Leap.Vector fingerPosition = frame.Hands.Frontmost.Fingers.Frontmost.TipPosition;
+				Leap.Vector fingerPosition = frame.Hands.Rightmost.Fingers[1].TipPosition;
+
 				
 				if (fingerPosition.Magnitude != 0) 
 				{
-					Leap.Vector fingerNormalized = leapToWorld (frame.Hands.Frontmost.Fingers.Frontmost.TipPosition,iBox);
+						HandController controllerGO = GetComponent<HandController>();
+						//Debug.Log (controllerGO.transform.position);
 						
-						float x = fingerPosition.x;
-						float y = fingerPosition.y;
-						float z = fingerPosition.z;
+						float x = -(fingerPosition.x/1000)+controllerGO.transform.position.x;
+						float y = (fingerPosition.y/1000)+controllerGO.transform.position.y;
+						float z = -(fingerPosition.z/1000)+controllerGO.transform.position.z;
 						
 
 						Vector3 position = new Vector3 (x, y, z);
-						Debug.Log (fingerNormalized);
-						Collider[] close_things = Physics.OverlapSphere (position, 20f, -1);
-						GameObject[] go = GameObject.FindGameObjectsWithTag("GrabbableObject");
-						Debug.Log("go: " + go[0].transform.position);
+						Debug.Log (position);
+						Collider[] close_things = Physics.OverlapSphere (position, .5f, -1);
+						//Debug.Log("go: " + go[0].transform.position);
 		
 						GameObject closest = null;
 						float closestDistance = Mathf.Infinity;
 					
 						for (int j = 0; j < close_things.Length; ++j) {
-								Debug.Log ("Cube " + close_things [0].transform.position);
+								//Debug.Log ("Cube " + close_things [0].transform.position);
 								if (close_things [j] != null && close_things[j].tag == "GrabbableObject") 
 								{
-									float dist = Vector3.Distance (position.normalized, close_things [j].transform.position);
-									Debug.Log ("Cube " + close_things [0].transform.position);
+									float dist = Vector3.Distance (position, close_things [j].transform.position);
+									//Debug.Log ("Cube " + close_things [0].transform.position);
 									if (closestDistance > dist) 
 									{
 										closest = close_things [j].gameObject;
@@ -53,18 +50,12 @@ public class ClosestObjectFinder : MonoBehaviour {
 									}
 								}
 						}
-						if (closestDistance < 0.2f) {
+						if (closestDistance < 0.1f) {
 								Debug.Log (closest.name.ToString ());
 						}
 					
 				}
 		}
 
-	Leap.Vector leapToWorld(Leap.Vector leapPoint, InteractionBox iBox)
-	{
-		leapPoint.z *= -1.0f; //right-hand to left-hand rule
-		Leap.Vector normalized = iBox.NormalizePoint(leapPoint, false);
-		normalized += new Leap.Vector(0.5f, 0f, 0.5f); //recenter origin
-		return normalized; //scale
-	}
+
 }
