c2ff173a783c4f27a4f827fddb09ff8ad10fa88d
Nindustrialist <nindustrialist@gmail.com>
2015-02-26 18:09:57 -0500

    Started wire logic
    Made logic for connecting targets, that is a little buggy

---
+++ Assets/ProjectScripts/ConnectorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectorController : MonoBehaviour {
+    
+    public GameObject prefab;
+    public TargetController start = null;
+    public TargetController end = null;
+    private Vector3 direction;
+    private GameObject wire;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.end != null)
+        {
+            direction = (end.transform.position - start.transform.position);
+            wire = Instantiate(prefab, start.transform.position, Quaternion.Euler(90, direction.z != 0 ? 0: 90, 0)) as GameObject;
+            wire.transform.localScale = direction;
+        }
+	}
+}
---
+++ Assets/ProjectScripts/TargetController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetController : MonoBehaviour {
+	public Light light;
+	GameObject controller;
+    GameObject gameInstantiated;
+	GameObject instantiated;
+
+	// Use this for initialization
+	void Start () 
+	{
+		light = GetComponent<Light> ();
+		controller = GameObject.FindGameObjectWithTag ("HandController");
+	}
+
+    GameObject PlaceObject(GameObject placingObject)
+    {
+        instantiated = Instantiate(placingObject, transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        instantiated.GetComponent<SelectedObject>().TurnOffLight();
+        instantiated.GetComponent<Collider>().enabled = false;
+        //instantiated.GetComponent<Rigidbody>().useGravity = false;
+        instantiated.GetComponent<SelectedObject>().enabled = false;
+
+        return instantiated;
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Collider[] close_things = Physics.OverlapSphere (transform.position, .075f, -1);
+		GameObject selected = null;
+		for(int i = 0; i < close_things.Length; i++)
+		{
+			//Debug.Log(close_things[i].ToString());
+			if(close_things[i].name == "palm")
+			{
+				selected = controller.GetComponent<ClosestObjectFinder>().selected;
+                if (selected != null)
+                {
+                    ConnectorController connector = selected.GetComponent<ConnectorController>();
+                    if (connector == null)
+                    {
+                        if (selected != null && instantiated == null)
+                        {
+                            light.intensity = 1;
+                            instantiated = PlaceObject(selected);
+                        }
+                        else if (selected != null && selected.name != instantiated.name)
+                        {
+                            Destroy(instantiated);
+                            instantiated = PlaceObject(selected);
+                        }
+                    }
+                    else
+                    {
+                        if (connector.start == null)
+                        {
+                            connector.start = this;
+                        }
+                        else if (connector.end == null && this != connector.start)
+                        {
+                            connector.end = this;
+                        }
+                    }
+                }
+			}
+		}
+	}
+}
