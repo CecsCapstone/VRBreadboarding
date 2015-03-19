831c549ebea0561c16c9d1db294a0087659d925e
Russell Parrott <rparrot93@gmail.com>
2015-03-19 09:36:41 -0400

    DeselectListener
    Somehow this didn't get included in the last commit...

---
+++ Assets/DeselectListener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hovercast.Core.Navigation;
+using Leap;
+
+public class DeselectListener : HovercastNavItemListener<NavItemRadio>
+{
+    protected override void Setup()
+    {
+        Item.OnValueChanged += HandleValueChanged;
+    }
+
+    protected override void BroadcastInitialValue()
+    {
+        HandleValueChanged(Item);
+    }
+
+    private void HandleValueChanged(NavItem<bool> pNavItem)
+    {
+        GameObject controller = GameObject.FindGameObjectWithTag("HandController");
+        ClosestObjectFinder finder = controller.GetComponent<ClosestObjectFinder>();
+        if (!pNavItem.Value)
+        {
+            return;
+        }
+        Debug.Log(pNavItem);
+
+        if (finder.selected != null)
+        {
+            finder.selected.GetComponent<SelectedObject>().Deselect();
+            controller.GetComponent<TargetSelectController>().enabled = false;
+            if (finder.selected.GetComponent<ConnectorController>() != null && finder.selected.GetComponent<ConnectorController>().start != null)
+            {
+                finder.selected.GetComponent<ConnectorController>().Reset();
+            }
+            finder.selected = null;
+        }
+    }
+}
