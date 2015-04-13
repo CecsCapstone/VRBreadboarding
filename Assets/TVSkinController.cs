005230c52fea35c29dcc7e4d1f02cc5ba3328df9
nltcardfan1 <nltcardfan1@gmail.com>
2015-04-13 10:21:19 -0400

    forgot one

--- Assets/TVSkinController.cs
+++ Assets/TVSkinController.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class TVSkinController : MonoBehaviour {
 
-	// Use this for initialization
-
 	public Material blackTv;
 	public Material collinTv;
 	public TextMesh voltageText;
 	public TextMesh resistanceText;
 	public TextMesh currentText;
-	void Start () {
-	
-	}
 
 	public void UpdateTV(float voltage,float resistance,float current)
 	{
@@ -22,14 +17,20 @@ public class TVSkinController : MonoBehaviour {
 		voltageText.text = "Current: " + current.ToString("0.##") + "Amps";
 	}
 
-	public void MakeTvBlack()
+	private void MakeTvBlack()
 	{
+		if(renderer.material != blackTv)
 		renderer.material = blackTv;
 	}
 
 	public void MakeTvCollin()
 	{
+		if(renderer.material !=collinTv)
 		renderer.material = collinTv;
+
+		voltageText.text = "";
+		resistanceText.text = "";
+		voltageText.text = "";
 	}
 	
 	
